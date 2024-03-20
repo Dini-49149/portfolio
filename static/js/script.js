05f02a62ce90230e4f67bbc62045bbbb5d692eaa
Dinesh Vennapoosa <71541898+Dini-49149@users.noreply.github.com>
2024-03-20 13:00:01 -0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -34,5 +34,4 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 const headerLogoConatiner = document.querySelector('.header__logo-container')
 
 headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
 })
